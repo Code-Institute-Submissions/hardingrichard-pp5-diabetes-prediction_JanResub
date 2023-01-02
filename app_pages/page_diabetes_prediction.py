3a708ca5b03399ead185209ccde2af6ae0fd7079
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 20:13:57 +0000

    Add placeholder code for page load

---
+++ app_pages/page_diabetes_prediction.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+def page_diabetes_prediction_body():
+    """
+    Function that loads the variable and runs the elements
+    within the Diabetes Prediction dashboard page.
+    """
+
+    st.write("## Diabetes Prediction:")
+    st.info(
+        "Placeholder text"
+    )
