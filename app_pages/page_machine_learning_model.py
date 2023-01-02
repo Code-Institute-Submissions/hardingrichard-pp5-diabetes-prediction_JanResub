643c081ea46b9df0a9471a6b3d6f7c426ca3df7d
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 20:18:27 +0000

    Add placeholder text so that page will load in previewer

---
+++ app_pages/page_machine_learning_model.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+def page_machine_learning_model_body():
+    """
+    Function that loads the variable and runs the elements
+    within the Diabetes Prediction dashboard page.
+    """
+
+    st.write("## Machine Learning Model:")
+    st.info(
+        "Placeholder text"
+    )
