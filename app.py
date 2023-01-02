6da32f5a83846c1dfe14f413d8f6267a6e3adf43
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 00:30:08 +0000

    Add app.y and import page scripts

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlist as st
+from app_pages.multipage import MultiPage
+
+# Loads scripts for the dashboard pages
+from app_pages.page_introduction import page_introduction_body
+from app_pages.page_correlation_study import page_correlation_study_body
+from app_pages.page_hypothesis import page_hypothesis_body
+from app_pages.page_machine_learning_model import page_machine_learning_model_body
+from app_pages.page_diabetes_prediction import page_diabetes_prediction_body
