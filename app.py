79e08ff018bf41c6b4f251d2541b06edf8376c6d
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 20:14:56 +0000

    Refactor code formatting

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from app_pages.page_introduction import page_introduction_body
 # from app_pages.page_correlation_study import page_correlation_study_body
 # from app_pages.page_hypothesis import page_hypothesis_body
 # from app_pages.page_machine_learning_model import page_machine_learning_model_body
-# from app_pages.page_diabetes_prediction import page_diabetes_prediction_body
+from app_pages.page_diabetes_prediction import page_diabetes_prediction_body
 
 # Creates an instance of the app
 app = MultiPage(app_name = "Diabetes Prediction")
@@ -16,6 +16,6 @@ app.add_page("Introduction", page_introduction_body)
 # app.add_page("Diabetes Correlation Study", page_correlation_study_body)
 # app.add_page("Project Hypothesis", page_hypothesis_body)
 # app.add_page("Machine Learning Model", page_machine_learning_model_body)
-# app.add_page("Diabetes Predictor", page_diabetes_prediction_body)
+app.add_page("Diabetes Predictor", page_diabetes_prediction_body)
 
 app.run()
