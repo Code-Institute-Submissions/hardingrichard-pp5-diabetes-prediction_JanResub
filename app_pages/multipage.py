7dbb430b86987f1aadb31e4935474626365d219b
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 00:21:10 +0000

    Add multipage.py app for creating new streamlit dashboard pages

---
+++ app_pages/multipage.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+class MultiPage:
+    """
+    Class for generating and configuring additional streamlit app pages using object orientation
+    """
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+        st.set_page_config(
+            page_title = self.app_name,
+            page_icon = '📄' # Sourced from the following website: https://twemoji.maxcdn.com/v/latest/svg/1f4c4.svg
+        )
+    
+    # Function for creeating a new page
+    def add_page(self, title, func) -> None:
+        self.pages.append({'title': title, 'function': func})
+    
+    # Creates a new instance in the sidebar with the new page title
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Contents', self.pages, format_func = lambda page: page['title'])
+        page['function']()
