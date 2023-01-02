9fffb0c924b948e911be8ee413f8857fe624d5f5
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 00:38:18 +0000

    Add the creation of app pages for the dashboard sidebar

--- app.py
+++ app.py
@@ -8,4 +8,14 @@ from app_pages.page_hypothesis import page_hypothesis_body
 from app_pages.page_machine_learning_model import page_machine_learning_model_body
 from app_pages.page_diabetes_prediction import page_diabetes_prediction_body
 
+# Creates an instance of the app
 app = MultiPage(app_name = "Diabetes Prediction")
+
+# Create app pages for the dashboard in the sidebar
+app.add_page("Introduction", page_introduction_body)
+app.add_page("Diabetes Correlation Study", page_correlation_study_body)
+app.add_page("Project Hypothesis", page_hypothesis_body)
+app.add_page("Machine Learning Model", page_machine_learning_model_body)
+app.add_page("Diabetes Predictor", page_diabetes_prediction_body)
+
+app.run()
