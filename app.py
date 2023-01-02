576fca7a3d20a327046c232eff25426c7c833ea4
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 00:31:13 +0000

    Add app variable for the app name using MultiPage class

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ from app_pages.page_correlation_study import page_correlation_study_body
 from app_pages.page_hypothesis import page_hypothesis_body
 from app_pages.page_machine_learning_model import page_machine_learning_model_body
 from app_pages.page_diabetes_prediction import page_diabetes_prediction_body
+
+app = MultiPage(app_name = "Diabetes Prediction")
