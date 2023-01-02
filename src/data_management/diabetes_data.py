ea01273c130bb4cb61f70877df0d41e46f212d46
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-02 20:59:40 +0000

    Add function to load diabetes dataset for dashboard use

---
+++ src/data_management/diabetes_data.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_diabetes_data():
+    """
+    Function which loads the Diabetes Dataset as a DataFrame
+    """
+
+    df = pd.read_csv("outputs/datasets/collection/diabetes.csv")
