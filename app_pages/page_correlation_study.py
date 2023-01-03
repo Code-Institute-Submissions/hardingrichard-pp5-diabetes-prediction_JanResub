9866021e79fc7ac9ea1bfa54d5d0640cb77cc141
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-03 02:13:39 +0000

    Add checkbox widget for inspecting the dataset

--- app_pages/page_correlation_study.py
+++ app_pages/page_correlation_study.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Loads the function in data_management for dataset
-from src.data_management import load_diabetes_data
+from src.data_management.diabetes_data import load_diabetes_data
 
 
 def page_correlation_study_body():
@@ -14,7 +14,24 @@ def page_correlation_study_body():
     To display the correlated features
     """
 
-    st.write("## Diabetes Correlation Study:")
+    df = load_diabetes_data()
+
+    st.write("## Diabetes Correlation Study: ")
     st.info(
-        "Placeholder text"
+        f"Business Requirement 1 - The client is interested in discovering how "
+        f"various biomarkers in female patients correlate between those with "
+        f"diabetes and without diabetes. The client expects to better "
+        f"understand this by reviewing data visualizations of the biomarker "
+        f"variables. "
     )
+    
+    # Inspecting the dataset
+    if st.checkbox("Inspect Diabetes Dataset"):
+        st.write(
+            f"The diabetes dataset contains {df.shape[0]} rows and "
+            f"{df.shape[1]} columns. 'Outcome' is the target variable, which "
+            f"we will be correlating feature sets against this variable. \n"
+            f"\nThe first 10 rows will be shown below."
+            )
+
+        st.write(df.head(10))
