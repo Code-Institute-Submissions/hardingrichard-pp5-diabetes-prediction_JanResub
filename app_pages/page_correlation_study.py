756f0bde6eb08320af47a5db006d46b2daee320f
Richard Harding <richard.jharding@hotmail.co.uk>
2023-01-03 01:34:49 +0000

    Amend Performance evaluation section to include context for PPS

--- app_pages/page_correlation_study.py
+++ app_pages/page_correlation_study.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Loads the function in data_management for dataset
+from src.data_management import load_diabetes_data
+
 
 def page_correlation_study_body():
     """
-    Function that loads the variable and runs the elements
-    within the Diabetes Prediction dashboard page.
+    Function that loads the elements within the correlation study page.
+    To display the correlated features
     """
 
     st.write("## Diabetes Correlation Study:")
